5f3be87362f4b69d889255816ba55b0686157984
dcifligu <deivi.cifligu@gmail.com>
2025-03-30 22:23:34 +0200

    Refactor code for improved performance

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Circles from './components/Circles.svg.vue'
+</script>
+
+<template>
+  <div class="bg-[#F6F6F6] bg-noise h-screen w-screen relative z-10 overflow-hidden">
+    <Circles class="absolute z-20"/>
+    <div class="h-full w-full relative z-30">
+      <div class="p-8 w-full h-full">
+        <div class="flex w-full h-full items-end">
+          <div class="bg-black w-1/4 h-fit p-2 rounded-full">
+            <div class="flex h-fit w-full items-center justify-between">
+              <div class="bg-white w-24 h-24 rounded-full"></div>
+              <div class="w-fit pr-4 h-fit flex flex-col items-center justify-center">
+                <span class="text-white text-4xl font-light uppercase">Click here</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-noise {
+  background-image: url("./assets/media/noise.png");
+  background-size: auto;
+  background-repeat: repeat;
+  background-blend-mode: multiply;
+}
+
+</style>
---
+++ src/components/Circles.svg.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { onMounted } from 'vue';
+import gsap from 'gsap';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+
+gsap.registerPlugin(MotionPathPlugin);
+
+onMounted(() => {
+  const circles = [1, 2, 3, 4, 5];
+  const reverseCircles = [1, 4]; // Define which circles should be reversed
+
+  circles.forEach(num => {
+    gsap.to(`#circle_${num}_dot`, {
+      duration: 4, 
+      repeat: -1, 
+      ease: "none", 
+      motionPath: {
+        path: `#circle_${num}_inner_path`, 
+        align: `#circle_${num}_inner_path`,
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5],
+        start: reverseCircles.includes(num) ? 1 : 0, // Reverse start and end for selected circles
+        end: reverseCircles.includes(num) ? 0 : 1
+      }
+    });
+  });
+});
+</script>
+
+<template>
+    <svg width="1440" height="1024" viewBox="0 0 1440 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <!-- Circle 1 -->
+        <path id="circle_1_stroke" d="M1100 264.5C1100 456.408 943.534 612 750.5 612C557.466 612 401 456.408 401 264.5C401 72.5918 557.466 -83 750.5 -83C943.534 -83 1100 72.5918 1100 264.5Z" stroke="#B2B2B2" stroke-width="4"/>
+        <path id="circle_1_inner_path" d="M1070 264.5C1070 436 930 572 750.5 572C571 572 431 436 431 264.5C431 93 571 -43 750.5 -43C930 -43 1070 93 1070 264.5Z" fill="none" stroke="none"/>
+        <circle id="circle_1_dot" cx="500.5" cy="473.5" r="7.5" fill="#1E1E1E"/>
+
+        <!-- Circle 2 -->
+        <path id="circle_2_stroke" d="M2077 332C2077 600.399 1858.08 818 1588 818C1317.92 818 1099 600.399 1099 332C1099 63.6012 1317.92 -154 1588 -154C1858.08 -154 2077 63.6012 2077 332Z" stroke="#B2B2B2" stroke-width="4"/>
+        <path id="circle_2_inner_path" d="M2047 332C2047 570 1838 788 1588 788C1338 788 1129 570 1129 332C1129 94 1338 -124 1588 -124C1838 -124 2047 94 2047 332Z" fill="none" stroke="none"/>
+        <circle id="circle_2_dot" cx="1136.5" cy="272.5" r="7.5" fill="#1E1E1E"/>
+
+        <!-- Circle 3 -->
+        <path id="circle_3_stroke" d="M1127 963.5C1127 1155.38 968.323 1311 772.5 1311C576.677 1311 418 1155.38 418 963.5C418 771.618 576.677 616 772.5 616C968.323 616 1127 771.618 1127 963.5Z" stroke="#B2B2B2" stroke-width="4"/>
+        <path id="circle_3_inner_path" d="M1097 963.5C1097 1135 947 1272 772.5 1272C598 1272 448 1135 448 963.5C448 792 598 655 772.5 655C947 655 1097 792 1097 963.5Z" fill="none" stroke="none"/>
+        <circle id="circle_3_dot" cx="1104.5" cy="993.5" r="7.5" fill="#1E1E1E"/>
+
+        <!-- Circle 4 -->
+        <path id="circle_4_stroke" d="M438 800.5C438 992.382 279.323 1148 83.5 1148C-112.323 1148 -271 992.382 -271 800.5C-271 608.618 -112.323 453 83.5 453C279.323 453 438 608.618 438 800.5Z" stroke="#B2B2B2" stroke-width="4"/>
+        <path id="circle_4_inner_path" d="M408 800.5C408 962 268 1108 83.5 1108C-101 1108 -241 962 -241 800.5C-241 639 -101 493 83.5 493C268 493 408 639 408 800.5Z" fill="none" stroke="none"/>
+        <circle id="circle_4_dot" cx="229.5" cy="519.5" r="7.5" fill="#1E1E1E"/>
+
+        <!-- Circle 5 -->
+        <path id="circle_5_stroke" d="M414 101.5C414 293.382 255.323 449 59.5 449C-136.323 449 -295 293.382 -295 101.5C-295 -90.3817 -136.323 -246 59.5 -246C255.323 -246 414 -90.3817 414 101.5Z" stroke="#B2B2B2" stroke-width="4"/>
+        <path id="circle_5_inner_path" d="M384 101.5C384 264 234 419 59.5 419C-115 419 -265 264 -265 101.5C-265 -61 -115 -216 59.5 -216C234 -216 384 -61 384 101.5Z" fill="none" stroke="none"/>
+        <circle id="circle_5_dot" cx="290.5" cy="339.5" r="7.5" fill="#1E1E1E"/>
+    </svg>        
+</template>
